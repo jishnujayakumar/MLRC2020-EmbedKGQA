35445e78d400c8d633ab3d89dff789fded620f39
jishnujayakumar <jishnu.jayakumar182@gmail.com>
2021-01-04 15:00:22 +0530

    FIX

--- KGQA/RoBERTa/dataloader.py
+++ KGQA/RoBERTa/dataloader.py
@@ -10,7 +10,7 @@ from collections import defaultdict
 from tqdm import tqdm
 import numpy as np
 from transformers import *
-from helpers import *
+
 
 class DatasetWebQSP(Dataset):
     def __init__(self, data, entities, entity2idx, transformer_name):
@@ -20,7 +20,23 @@ class DatasetWebQSP(Dataset):
         self.pos_dict = defaultdict(list)
         self.neg_dict = defaultdict(list)
         self.index_array = list(self.entities.keys())
-        self.tokenizer = get_tokenizer(transformer_name) #helper func
+        self.tokenizer = None
+        self.set_tokenizer(transformer_name)
+
+    def set_tokenizer(self, transformer_name):
+        if transformer_name == 'RoBERTa':
+            self.tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
+        elif transformer_name == 'XLNet':
+            self.tokenizer = XLNetTokenizer.from_pretrained('xlnet-base-cased')
+        elif transformer_name == 'ALBERT':
+            self.tokenizer = AlbertTokenizer.from_pretrained('albert-base-v2')
+        elif transformer_name == 'SentenceTransformer':
+            self.tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/bert-base-nli-mean-tokens")
+        elif transformer_name == 'Reformer':
+            self.tokenizer = ReformerTokenizer.from_pretrained('google/reformer-crime-and-punishment')
+        else:
+            print('Incorrect transformer specified:', transformer_name)
+            exit(0)
 
     def __len__(self):
         return len(self.data)
@@ -45,10 +61,10 @@ class DatasetWebQSP(Dataset):
         data_point = self.data[index]
         question_text = data_point[1]
 
-        encoded_question = tokenize_question(question_text)
+        encoded_question = self.tokenize_question(question_text)
         question_tokenized=encoded_question['input_ids']
         attention_mask=encoded_question['attention_mask']
-        
+
         head_id = self.entity2idx[data_point[0].strip()]
         tail_ids = []
         for tail_name in data_point[2]:
@@ -58,6 +74,20 @@ class DatasetWebQSP(Dataset):
                 tail_ids.append(self.entity2idx[tail_name])
         tail_onehot = self.toOneHot(tail_ids)
         return question_tokenized, attention_mask, head_id, tail_onehot 
+
+    def tokenize_question(self, question):
+        question = f"<s>{question}</s>"
+        
+        encoded_question = self.tokenizer.encode_plus(
+                                question, # Question to encode
+                                add_special_tokens = False, # Add '[CLS]' and '[SEP]', as per original paper
+                                max_length = 64,           # Pad & truncate all sentences.
+                                padding = 'max_length',
+                                return_attention_mask = True,   # Construct attn. masks.
+                                return_tensors = 'pt'     # Return pytorch tensors.
+                            )
+
+        return encoded_question
 
 # def _collate_fn(batch):
 #     print(len(batch))
