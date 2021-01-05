6403141bedf9c5a5101c172fe5147939178e2491
jishnujayakumar <jishnu.jayakumar182@gmail.com>
2021-01-05 20:37:38 +0530

    ADD LibKGE TuckER train config

--- scripts/install_libkge.sh
+++ scripts/install_libkge.sh
@@ -5,4 +5,5 @@ cd train_embeddings/
 git clone https://github.com/uma-pi1/kge.git && cd kge
 pip install -e .
 
-
+cp $EMBED_KGQA_DIR/scripts/preprocess_webqsp_dataset.sh data
+cp -R $EMBED_KGQA_DIR/data/fbwq_* data
