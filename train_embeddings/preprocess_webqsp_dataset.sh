47a0feaf35610a714bb39c61f8252a64863f62e1
jishnujayakumar <jishnu.jayakumar182@gmail.com>
2021-01-05 16:23:23 +0530

    FIX

---
+++ train_embeddings/preprocess_webqsp_dataset.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+BASEDIR=`$EMBED_KGQA_DIR/data`
+
+for webqsp_dataset in {'fbwq_half', 'fbwq_full'}; 
+do
+if [ ! -d "$BASEDIR/$webqsp_dataset" ]; then
+    echo "\"$webqsp_dataset\" dataset not found. Kindly download it by following steps mentioned in getting-started section [README]."
+else
+    echo "\"$webqsp_dataset\" is present."
+
+if [ ! -f "$BASEDIR/$webqsp_dataset/dataset.yaml" ]; then
+    python preprocess/preprocess_default.py data/$webqsp_dataset/
+else
+    echo "\"$webqsp_dataset\" already prepared."
+fi
+done
