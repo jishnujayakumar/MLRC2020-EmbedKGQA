b0b2da12394412d73ad53ba6b1a4a82c9d1892f4
jishnujayakumar <jishnu.jayakumar182@gmail.com>
2021-01-05 19:44:33 +0530

    UPDATE: FIX

--- scripts/install_libkge.sh
+++ scripts/install_libkge.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Retriev and install LibKGE in development mode
+# Retrieve and install LibKGE in development mode
 cd train_embeddings/
 git clone https://github.com/uma-pi1/kge.git && 
 pip install -e .
+
+
