cc95fafca198bb2479233645dfb50cfc7686209a
jishnujayakumar <jishnu.jayakumar182@gmail.com>
2021-01-06 18:05:16 +0530

    UPDATE: SentenceTransformer

--- KGQA/RoBERTa/helpers.py
+++ KGQA/RoBERTa/helpers.py
@@ -1,3 +1,12 @@
+# SentenceTransformer Mean Pooling - Take attention mask into account for correct averaging
+def mean_pooling(model_output, attention_mask):
+    token_embeddings = model_output[0] #First element of model_output contains all token embeddings
+    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+    sum_embeddings = torch.sum(token_embeddings * input_mask_expanded, 1)
+    sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+    return sum_embeddings / sum_mask
+
+
 def get_pretrained_model_name(transformer_name):
         if transformer_name == 'RoBERTa':
             return 'roberta-base'
