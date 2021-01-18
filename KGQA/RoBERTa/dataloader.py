ea3bf9d715b5241ff768538400e20037f38940e7
jishnujayakumar <jishnu.jayakumar182@gmail.com>
2021-01-18 19:22:12 +0530

    UPDATE: FIX

--- KGQA/RoBERTa/dataloader.py
+++ KGQA/RoBERTa/dataloader.py
@@ -95,7 +95,7 @@ class DatasetWebQSP(Dataset):
 
             return question_tokenized, torch.tensor(attention_mask, dtype=torch.long)
         else:
-            encoded_que = self.tokenizer.encode_plus(
+            encoded_que = self.tokenizer.encode(
             question,
             max_length=self.max_length,
             add_special_tokens=False, # Add '[CLS]' and '[SEP]'
@@ -104,7 +104,7 @@ class DatasetWebQSP(Dataset):
             return_attention_mask=True,
             return_tensors='pt'  # Return PyTorch tensors
             )
-            return encoded_que['input_ids'].flatten(), encoded_que['attention_mask'].flatten()
+            return encoded_que['input_ids'], encoded_que['attention_mask']
 
 class DataLoaderWebQSP(DataLoader):
     def __init__(self, *args, **kwargs):
