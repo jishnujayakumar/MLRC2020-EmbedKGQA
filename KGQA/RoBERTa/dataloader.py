18654857a989ca583e2db969a7beb6e49bc3494f
jishnujayakumar <jishnu.jayakumar182@gmail.com>
2021-01-18 21:27:54 +0530

    UPDATE: FIX

--- KGQA/RoBERTa/dataloader.py
+++ KGQA/RoBERTa/dataloader.py
@@ -96,7 +96,7 @@ class DatasetWebQSP(Dataset):
             return question_tokenized, torch.tensor(attention_mask, dtype=torch.long)
         else:
 
-            encoded_que = self.tokenizer.encode_plus(text,
+            encoded_que = self.tokenizer.encode_plus(question,
                                         max_length=self.max_length,
                                         add_special_tokens = False,
                                         pad_to_max_length=True,
