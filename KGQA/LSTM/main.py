d47a831161b51611c8baddbc297153e80a7a4a3f
jishnujayakumar <jishnu.jayakumar182@gmail.com>
2021-01-20 15:34:38 +0530

    UPDATE eval: MetaQA

--- KGQA/LSTM/main.py
+++ KGQA/LSTM/main.py
@@ -270,7 +270,7 @@ def perform_experiment(data_path, mode, entity_path, relation_path, entity_dict,
 
         model = RelationExtractor(embedding_dim=embedding_dim, hidden_dim=hidden_dim, vocab_size=len(word2ix), num_entities = len(idx2entity), relation_dim=relation_dim, pretrained_embeddings=embedding_matrix, freeze=freeze, device=device, entdrop = entdrop, reldrop = reldrop, scoredrop = scoredrop, l3_reg = l3_reg, model = model_name, ls = ls, w_matrix = w_matrix, bn_list=bn_list)
 
-        model_chkpt_file_path=get_checkpoint_file_path(checkpoint_path, model_name, num_hops, '', kg_type)+ '_' + 'best_score_model' + get_chk_suffix()
+        model_chkpt_file_path=get_checkpoint_file_path(checkpoint_path, model_name, num_hops, '', kg_type)+get_chk_suffix()
         model.load_state_dict(torch.load(model_chkpt_file_path, map_location=lambda storage, loc: storage))
         model.to(device)
         for parameter in model.parameters():
@@ -341,7 +341,6 @@ if args.kg_type == 'half':
     data_path = '../../data/QA_data/MetaQA/qa_train_' + hops + '_half.txt'
 else:
     data_path = '../../data/QA_data/MetaQA/qa_train_' + hops + '.txt'
-print('Train file is ', data_path)
 
 valid_data_path = '../../data/QA_data/MetaQA/qa_dev_' + hops + '.txt'
 test_data_path = '../../data/QA_data/MetaQA/qa_test_' + hops + '.txt'
