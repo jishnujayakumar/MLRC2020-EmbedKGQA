967e194864853372edc381a181ca43cd5eebc246
jishnujayakumar <jishnu.jayakumar182@gmail.com>
2021-01-21 09:54:07 +0530

    UPDATE eval: MetaQA

--- KGQA/RoBERTa/main.py
+++ KGQA/RoBERTa/main.py
@@ -18,6 +18,7 @@ import pandas as pd
 sys.path.append("../..") # Adds higher directory to python modules path.
 from kge.model import KgeModel
 from kge.util.io import load_checkpoint
+from torch.nn.utils.rnn import pad_sequence
 
 def str2bool(v):
     if isinstance(v, bool):
@@ -309,6 +310,15 @@ def getEntityEmbeddings(model_name, kge_model, hops):
 def get_chkpt_path(model_name, que_embedding_model, outfile):
     return f"../../checkpoints/WebQSP/{model_name}_{que_embedding_model}_{outfile}/best_score_model.pt"
 
+def pad_x_collate_function(batch):
+    # batch looks like [(x0,y0), (x4,y4), (x2,y2)... ]
+    xs = [sample[0] for sample in batch]
+    ys = [sample[1] for sample in batch] 
+    # If you want to be a little fancy, you can do the above in one line 
+    # xs, ys = zip(*samples) 
+    xs = pad_sequence(xs, batch_first=True, padding_value=0)
+    return xs, torch.tensor(ys)
+
 def perform_experiment(data_path, mode, neg_batch_size, batch_size, shuffle, num_workers, nb_epochs, embedding_dim, hidden_dim, relation_dim, gpu, use_cuda,patience, freeze, validate_every, hops, lr, entdrop, reldrop, scoredrop, l3_reg, model_name, decay, ls, load_from, outfile, do_batch_norm, que_embedding_model, valid_data_path=None, test_data_path=None):
     webqsp_checkpoint_folder = f"../../checkpoints/WebQSP/{model_name}_{que_embedding_model}_{outfile}/"
     if not os.path.exists(webqsp_checkpoint_folder):
@@ -340,7 +350,7 @@ def perform_experiment(data_path, mode, neg_batch_size, batch_size, shuffle, num
     if mode=='train':
         data = process_text_file(data_path)
         dataset = DatasetWebQSP(data, e, entity2idx, que_embedding_model, model_name)
-        data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+        data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, collate_fn=pad_x_collate_function)
         if load_from != '':
             # model.load_state_dict(torch.load("checkpoints/roberta_finetune/" + load_from + ".pt"))
             fname = f"checkpoints/{que_embedding_model}_finetune/{load_from}.pt"
