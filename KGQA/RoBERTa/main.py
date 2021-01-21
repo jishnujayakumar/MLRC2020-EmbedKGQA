852dddc8db6ebd48f13bb3e5b1b8d72a92f9dc95
jishnujayakumar <jishnu.jayakumar182@gmail.com>
2021-01-21 10:10:50 +0530

    UPDATE eval: MetaQA

--- KGQA/RoBERTa/main.py
+++ KGQA/RoBERTa/main.py
@@ -311,13 +311,14 @@ def get_chkpt_path(model_name, que_embedding_model, outfile):
     return f"../../checkpoints/WebQSP/{model_name}_{que_embedding_model}_{outfile}/best_score_model.pt"
 
 def pad_x_collate_function(batch):
-    # batch looks like [(x0,y0), (x4,y4), (x2,y2)... ]
     xs = [sample[0] for sample in batch]
-    ys = [sample[1] for sample in batch] 
-    # If you want to be a little fancy, you can do the above in one line 
-    # xs, ys = zip(*samples) 
+    ys = [sample[1] for sample in batch]
+
     xs = pad_sequence(xs, batch_first=True, padding_value=0.001)
-    return xs, ys
+
+    print(xs, ys)
+
+    return xs, torch.tensor(ys)
 
 def perform_experiment(data_path, mode, neg_batch_size, batch_size, shuffle, num_workers, nb_epochs, embedding_dim, hidden_dim, relation_dim, gpu, use_cuda,patience, freeze, validate_every, hops, lr, entdrop, reldrop, scoredrop, l3_reg, model_name, decay, ls, load_from, outfile, do_batch_norm, que_embedding_model, valid_data_path=None, test_data_path=None):
     webqsp_checkpoint_folder = f"../../checkpoints/WebQSP/{model_name}_{que_embedding_model}_{outfile}/"
