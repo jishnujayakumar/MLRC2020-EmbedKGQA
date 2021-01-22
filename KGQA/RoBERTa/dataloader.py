d438a95a4c3ae4fddfeab5ac85e71fec154f3486
jishnujayakumar <jishnu.jayakumar182@gmail.com>
2021-01-22 14:53:56 +0530

    ADD relational_tucker_3 config

--- KGQA/RoBERTa/dataloader.py
+++ KGQA/RoBERTa/dataloader.py
@@ -79,6 +79,7 @@ class DatasetWebQSP(Dataset):
         question = f"<s>{question}</s>"
         question_tokenized = self.tokenizer.tokenize(question)
         question_tokenized = self.pad_sequence(question_tokenized, self.max_length)
+        
         question_tokenized = torch.tensor(self.tokenizer.encode(
                                 question_tokenized, # Question to encode
                                 add_special_tokens = False # Add '[CLS]' and '[SEP]', as per original paper
