7f86dad759a0947eb101ec1b29ab51bd22858146
jishnujayakumar <jishnu.jayakumar182@gmail.com>
2021-01-27 08:14:31 +0530

    UDPATE

--- KGQA/RoBERTa/dataloader.py
+++ KGQA/RoBERTa/dataloader.py
@@ -99,8 +99,8 @@ class DatasetWebQSP(Dataset):
             print(question, type(question))
             print([question], type([question]))
 
-            encoded_que = self.tokenizer.encode([question], padding=True, truncation=True, max_length=self.max_length, return_tensors='pt')
-            return encoded_que['input_ids'], encoded_que['attention_mask']
+            encoded_que = self.tokenizer.encode(question, padding=True, truncation=True, max_length=self.max_length, return_tensors='pt')
+            return encoded_que['input_ids'][0], encoded_que['attention_mask'][0]
 
 # def _collate_fn(batch):
 #     print(len(batch))
